4af64d2fb43c4ab62ec9a85ba04335650138e7e2
Fernando <manuelhola9@gmail.com>
2024-12-07 08:48:11 -0800

    Agregar páginas de dashboard y configuración de Prisma

---
+++ src/app/dashboard/reservas/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello Reservaciones</h1>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
   return (
-
-    <div>
-      <h1>hola</h1>
-    </div>
+    redirect("dashboard/home")
   );
 }
