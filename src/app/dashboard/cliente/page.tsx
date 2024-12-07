4224adf89089c15899781b845a4247468519d78b
Fernando <manuelhola9@gmail.com>
2024-12-07 09:53:13 -0800

    Agregar componentes de botón y tabla, y actualizar dependencias de Radix UI

--- src/app/dashboard/cliente/page.tsx
+++ src/app/dashboard/cliente/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button"
 
 export default function Page() {
   return (
-    <div>
-      <h1>Hello Cliente</h1>
+    <div className="flex justify-end gap-2.5">
+      <Button>Create</Button>
+      <Button>Read</Button>
+      <Button>Update</Button>
+      <Button>Delete</Button>
     </div>
   );
 }
