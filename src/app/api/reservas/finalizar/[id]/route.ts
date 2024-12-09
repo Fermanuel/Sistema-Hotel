5257604efe61e26106e7fd594119d156b4f0048f
Fernando <manuelhola9@gmail.com>
2024-12-08 16:38:49 -0800

    Actualizar lógica de finalización de reservaciones para incluir verificación de existencia y actualizar el estado de la habitación a "disponible".

--- src/app/api/reservas/finalizar/[id]/route.ts
+++ src/app/api/reservas/finalizar/[id]/route.ts
@@ -1,4 +1,3 @@
-// /api/reservas/finalizar/[id].ts
 import { NextResponse } from "next/server";
 import prisma from "../../../../../lib/db";
 
@@ -9,6 +8,16 @@ export async function PATCH(req: Request, { params }: { params: { id: string } }
     // Obtener la fecha y hora actuales
     const fechaActual = new Date();
 
+    // Primero, obtener la reservación para acceder a la habitación asociada
+    const reserva = await prisma.reserva.findUnique({
+      where: { id: parseInt(id) },
+      include: { habitacion: true } // Incluir la habitación relacionada
+    });
+
+    if (!reserva) {
+      return NextResponse.json({ message: "Reservación no encontrada." }, { status: 404 });
+    }
+
     // Actualizar la reservación con `checkOut` y marcarla como finalizada
     const reservaActualizada = await prisma.reserva.update({
       where: { id: parseInt(id) },
@@ -16,6 +25,12 @@ export async function PATCH(req: Request, { params }: { params: { id: string } }
         finalizada: true, 
         checkOut: fechaActual, // Actualizar el campo `checkOut`
       },
+    });
+
+    // Luego, actualizar el estado de la habitación a "disponible"
+    await prisma.habitacion.update({
+      where: { id: reserva.habitacion.id }, // Usamos el ID de la habitación asociada a la reservación
+      data: { estado: "disponible" }, // Establecer el estado a "disponible"
     });
 
     return NextResponse.json(reservaActualizada, { status: 200 });
